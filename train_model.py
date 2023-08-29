d4b7d14c3b4a51e2cdde11828aa6c7768d693694
Nico Benz <nico.benz@studserv.uni-leipzig.de>
2023-08-29 13:46:32 +0200

    added cross-validation to bayes and log reg models

--- train_model.py
+++ train_model.py
@@ -11,10 +11,11 @@ from sklearn.metrics import classification_report
 from keras.models import Sequential
 from keras.layers import Embedding, LSTM, Dense
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import KFold
 import tensorflow as tf
 
 
-def prepare_tfidf(data, test_size=0.2):
+def prepare_tfidf(data, folds=5):
     # create lists with token strings and labels
     token_list = []
     label_list = []
@@ -24,6 +25,25 @@ def prepare_tfidf(data, test_size=0.2):
             token_list.append(toks)
             label_list.append(label)
 
+    # randomised k-folds
+    kf = KFold(n_splits=folds, shuffle=True, random_state=42)
+
+    for train_index, val_index in kf.split(token_list):
+        X_train, X_val = [token_list[i] for i in train_index], [token_list[i] for i in val_index]
+        y_train, y_val = [label_list[i] for i in train_index], [label_list[i] for i in val_index]
+
+        # Initialize the TF-IDF vectorizer and fit it on the entire dataset
+        tfidf_vectorizer = TfidfVectorizer()
+        tfidf_vectorizer.fit(token_list)
+
+        # Transform X_train and X_val into TF-IDF vectors
+        X_train_tfidf = tfidf_vectorizer.transform(X_train)
+        X_val_tfidf = tfidf_vectorizer.transform(X_val)
+
+        yield X_train_tfidf, X_val_tfidf, y_train, y_val
+
+
+"""
     # separate sets
     X_train, X_test, y_train, y_test = train_test_split(
         token_list,
@@ -38,50 +58,39 @@ def prepare_tfidf(data, test_size=0.2):
     X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
     return X_train_tfidf, X_test_tfidf, y_train, y_test
+"""
 
 
-def naive_bayes(data):
+def naive_bayes(data, folds=5):
     print("Naive Bayes")
-    # prepare lists for training
-    X_train, X_test, y_train, y_test = prepare_tfidf(data)
+    collected_metrics = []
+    # iterate over folds
+    for fold, (X_train, X_test, y_train, y_test) in enumerate(prepare_tfidf(data, folds), start=1):
+        nb_classifier = MultinomialNB()
+        print(f"Fitting fold {fold}...")
+        nb_classifier.fit(X_train, y_train)
+        y_pred = nb_classifier.predict(X_test)
 
-    nb_classifier = MultinomialNB()
-    print("Fitting...")
-    nb_classifier.fit(X_train, y_train)
+        report = classification_report(y_test, y_pred, output_dict=True)
+        collected_metrics.append(report)
 
-    print("Predicting...")
-    y_pred = nb_classifier.predict(X_test)
-
-    print("Results:")
-    model_evaluation_overview("naive_bayes.json", y_test, y_pred)
-
-    report = classification_report(y_test, y_pred)
-
-    # save classification report to a file
-    with open("naive_bayes_full_report.txt", "w") as f:
-        f.write(report)
+    mean_folding_report(collected_metrics, "naive_bayes_full_report.json")
 
 
-def logistic_regression(data):
+def logistic_regression(data, folds=5):
     print("Logistic Regression")
-    # prepare lists for training
-    X_train, X_test, y_train, y_test = prepare_tfidf(data)
+    collected_metrics = []
+    # iterate over folds
+    for fold, (X_train, X_test, y_train, y_test) in enumerate(prepare_tfidf(data, folds), start=1):
+        nb_classifier = MultinomialNB()
+        print(f"Fitting fold {fold}...")
+        nb_classifier.fit(X_train, y_train)
+        y_pred = nb_classifier.predict(X_test)
 
-    clf = LogisticRegression(max_iter=1000)
-    print("Fitting...")
-    clf.fit(X_train, y_train)
+        report = classification_report(y_test, y_pred, output_dict=True)
+        collected_metrics.append(report)
 
-    print("Predicting...")
-    y_pred = clf.predict(X_test)
-
-    print("Results:")
-    model_evaluation_overview("log_reg.json", y_test, y_pred)
-
-    report = classification_report(y_test, y_pred)
-
-    # save classification report to a file
-    with open("log_reg_full_report.txt", "w") as f:
-        f.write(report)
+    mean_folding_report(collected_metrics, "log_reg_full_report.json")
 
 
 def random_forest(data):
@@ -102,7 +111,7 @@ def random_forest(data):
     report = classification_report(y_test, y_pred)
 
     # save classification report to a file
-    with open("random_forest_full_report.txt", "w") as f:
+    with open("/Volumes/Data/steam/results/random_forest_full_report.txt", "w") as f:
         f.write(report)
 
 
@@ -125,7 +134,7 @@ def support_vector_machine(data):
     report = classification_report(y_test, y_pred)
 
     # save classification report to a file
-    with open("svm_full_report.txt", "w") as f:
+    with open("/Volumes/Data/steam/results/svm_full_report.txt", "w") as f:
         f.write(report)
 
 
@@ -179,7 +188,7 @@ def recurrent_neural_network(data):
     report = classification_report(y_test, y_pred)
 
     # save classification report to a file
-    with open("rnn_full_report.txt", "w") as f:
+    with open("/Volumes/Data/steam/results/rnn_full_report.txt", "w") as f:
         f.write(report)
 
 
@@ -207,7 +216,53 @@ def model_evaluation_overview(file_name, test, pred, print_results=True):
     # save to disk
     save_path = f"/Volumes/Data/steam/results/{file_name}"
     with open(save_path, "w") as file_out:
-        json.dump(eval_dict,file_out)
+        json.dump(eval_dict, file_out)
+
+
+def mean_folding_report(metrics_data, filename, print_results=True):
+    metrics = {}
+    for item in metrics_data:
+        for key, value in item.items():
+            # print(key, value)
+            if key == "accuracy":
+                if key in metrics:
+                    metrics[key].append(value)
+                else:
+                    metrics[key] = [value]
+            else:
+                if key in metrics:
+                    for k, v in value.items():
+                        metrics[key][k].append(v)
+                else:
+                    metrics[key] = {}
+                    for k, v in value.items():
+                        metrics[key][k] = [v]
+        # print("---")
+    mean_metrics = {"labels": {}, "combined": {}}
+    for key, value in metrics.items():
+        if key == "accuracy":
+            mean_metrics["combined"][key] = round(sum(value) / len(value), 2)
+        elif key == "macro avg" or key == "weighted avg":
+            mean_metrics["combined"][key] = {}
+            for k, v in value.items():
+                mean_metrics["combined"][key][k] = round(sum(v) / len(v), 2)
+        else:
+            mean_metrics["labels"][key] = {}
+            for k, v in value.items():
+                mean_metrics["labels"][key][k] = round(sum(v) / len(v), 2)
+
+    with open(f"/Volumes/Data/steam/results/{filename}", "w") as file_out:
+        json.dump(mean_metrics, file_out)
+
+    if print_results:
+        print("\nMean Metrics across all folds:")
+        for key, val in mean_metrics["combined"].items():
+            if key == "accuracy":
+                print(f"{key}: {val}")
+            else:
+                print(key)
+                for k, v in val.items():
+                    print(f"-- {k}: {v}")
 
 
 # load token
@@ -215,8 +270,9 @@ with open("/Volumes/Data/steam/finished_corpus/corpus.json", "r") as file_in:
     token_data = json.load(file_in)
 
 # train models
-#support_vector_machine(token_data)
-#random_forest(token_data)
-#logistic_regression(token_data)
-#naive_bayes(token_data)
-recurrent_neural_network(token_data)
+# support_vector_machine(token_data)
+# random_forest(token_data)
+logistic_regression(token_data)
+# naive_bayes(token_data)
+# recurrent_neural_network(token_data)
+
