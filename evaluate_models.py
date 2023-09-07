c15755fc67100d5927f57e471d6e652b0d719d0c
Nico Benz <nico.benz@studserv.uni-leipzig.de>
2023-09-07 14:45:19 +0200

    added roc curves for models and updated tex tables and graphics implementation

--- evaluate_models.py
+++ evaluate_models.py
@@ -4,12 +4,26 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import roc_curve, auc
 import seaborn as sns
+import locale
 
 
-"""
-to-do:
-- model metrics table
-"""
+def save_tex(file_name, template):
+    with open(f"tex/{file_name}", "w", encoding="utf-8") as latex_out:
+        latex_out.write(template)
+
+
+def load_tex(file_name):
+    with open(f"tex_templates/{file_name}", "rb") as template_in:
+        template = template_in.read().decode('utf-8', 'ignore')
+    return template
+
+
+def format_numbers_for_readability(number_list):
+    number_list = [
+            locale.format_string("%.2f", number, grouping=True) if isinstance(number, float) else
+            locale.format_string("%d", number, grouping=True) for number in number_list
+        ]
+    return number_list
 
 
 def roc(source_path):
@@ -26,11 +40,9 @@ def roc(source_path):
 
     # loop over all prediction files
     for file in pred_files:
-        print(file)
         with open(file, "r") as pred_in:
             predictions = json.load(pred_in)
         save_name = file.split("/")[-1].replace("_predictions.json", "")
-        print(save_name)
         model_name = save_name.split("_")
         model_name = [x.capitalize() for x in model_name]
         model_name = " ".join(model_name)
@@ -85,12 +97,34 @@ def roc(source_path):
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title(f'Receiver Operating Characteristic One-vs-Rest (ROC OvR) for {model_name}')
+        #plt.title(f'Receiver Operating Characteristic (ROC), One-vs-Rest (OvR) for {model_name}')
+        plt.title("")
         plt.legend(loc='lower right')
         plt.savefig(f"{source_path}/results/plots/{save_name}_roc.png", dpi=600)
 
 
-def put_metrics_into_latex_tables():
+def plot_aggregated_learning_curve(source_path, save_string):
+    with open(f"{source_path}/results/learning_curve_data/{save_string}_learning_curve_data.json", "r", encoding="utf-8") as lc_in:
+        learning_curve_data = json.load(lc_in)
+
+    # Calculate the aggregated learning curve
+    aggregated_train_sizes = np.mean(learning_curve_data['train_sizes'], axis=0)
+    aggregated_train_scores = np.mean(learning_curve_data['train_scores'], axis=0)
+    aggregated_test_scores = np.mean(learning_curve_data['test_scores'], axis=0)
+
+    # Plot the aggregated learning curve
+    plt.figure(figsize=(8, 6))
+    plt.plot(aggregated_train_sizes, aggregated_train_scores, label='Training Accuracy', marker='o')
+    plt.plot(aggregated_train_sizes, aggregated_test_scores, label='Test Accuracy', marker='o')
+    plt.xlabel('Number of Training Samples')
+    plt.ylabel('Accuracy')
+    plt.title('Aggregated Learning Curve')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"{source_path}/results/plots/{save_string}_learning_curve.png", dpi=600)
+
+
+def put_model_metrics_into_latex_tables():
     # table for all models and all labels
     path = "data/results/model_metrics"
     saved_models = [f"{path}/{x}" for x in os.listdir(path) if ".DS_Store" not in x]
@@ -102,11 +136,14 @@ def put_metrics_into_latex_tables():
         model_data = []
         name = m.split("/")[-1]
         name = name.replace("_full_report.json", "")
+        fig_name = name.split("_")[1:]
+        fig_name = "_".join(fig_name)
+        print(fig_name)
         name = name.replace(f"{idx}_", "")
         name = name.split("_")
         name = [token.capitalize() for token in name]
         name = ' '.join(name)
-        model_names.append(name)
+        model_names.append((name, fig_name))
         with open(m, "r") as file_in:
             content = json.load(file_in)
             aggregated = list(content["combined"]["macro avg"].values())
@@ -119,11 +156,12 @@ def put_metrics_into_latex_tables():
     for name, model in zip(model_names, model_metrics):
         with open("tex_templates/template_model_metrics_table.tex", "rb") as template_in:
             template = template_in.read().decode('utf-8', 'ignore')
-        template = template.replace("<name>", name)
+        template = template.replace("tab:model_metrics", f"tab:model_metrics_{name[1]}")
+        template = template.replace("<name>", name[0])
         for i, mod in enumerate(model):
             for j, value in enumerate(mod):
                 template = template.replace(f"<{i}{j}>", str(value))
-        file_label = name.split(" ")
+        file_label = name[0].split(" ")
         file_label = [label.lower() for label in file_label]
         file_label = "_".join(file_label)
 
@@ -148,5 +186,30 @@ def put_metrics_into_latex_tables():
         latex_out.write(template)
 
 
-#put_metrics_into_latex_tables()
+def put_review_metrics_into_latex_table():
+    file_names = [
+        "/Users/nico/code/GameStudies-SteamPredictions/data/results/descriptive_stats/all_token_metrics.json",
+        "/Users/nico/code/GameStudies-SteamPredictions/data/results/descriptive_stats/english_token_metrics.json"
+    ]
+    collected_metrics = []
+    for file in file_names:
+        with open(file, "r") as file_in:
+            metrics = json.loads(file_in.read())
+
+        metrics = list(metrics.values())
+        metrics = format_numbers_for_readability(metrics)
+        collected_metrics.append(metrics)
+
+    template = load_tex("template_review_metrics.tex")
+
+    for i, column in enumerate(collected_metrics):
+        for j, metric in enumerate(column):
+            template = template.replace(f"<{i}{j}>", str(metric))
+    save_tex("review_metrics.tex", template)
+
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+put_model_metrics_into_latex_tables()
 roc("data")
+put_review_metrics_into_latex_table()
--- preprocess_data.py
+++
@@ -1,3 +0,0 @@
-"""
-to-do: select data
-"""
